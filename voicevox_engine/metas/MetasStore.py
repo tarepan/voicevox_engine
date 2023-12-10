bffc6a79605050f213f98fd470babaee67d305e4
tarepan <tarepan5884@gmail.com>
2023-12-10 04:51:38 +0000

    Refactor: `MetasStore` メソッドのプライベート化と削除

--- voicevox_engine/metas/MetasStore.py
+++ voicevox_engine/metas/MetasStore.py
@@ -16,7 +16,6 @@ class MetasStore:
     """
 
     def __init__(self, engine_speakers_path: Path) -> None:
-        self._engine_speakers_path = engine_speakers_path
         self._loaded_metas: Dict[str, EngineSpeaker] = {
             folder.name: EngineSpeaker(
                 **json.loads((folder / "metas.json").read_text(encoding="utf-8"))
@@ -24,10 +23,10 @@ class MetasStore:
             for folder in engine_speakers_path.iterdir()
         }
 
-    def speaker_engine_metas(self, speaker_uuid: str) -> EngineSpeaker:
-        return self.loaded_metas[speaker_uuid]
+    def _speaker_engine_metas(self, speaker_uuid: str) -> EngineSpeaker:
+        return self._loaded_metas[speaker_uuid]
 
-    def combine_metas(self, core_metas: List[CoreSpeaker]) -> List[Speaker]:
+    def _combine_metas(self, core_metas: List[CoreSpeaker]) -> List[Speaker]:
         """
         与えられたmetaにエンジンのコア情報を付加して返す
         core_metas: コアのmetas()が返すJSONのModel
@@ -35,7 +34,7 @@ class MetasStore:
 
         return [
             Speaker(
-                **self.speaker_engine_metas(speaker_meta.speaker_uuid).dict(),
+                **self._speaker_engine_metas(speaker_meta.speaker_uuid).dict(),
                 **speaker_meta.dict(),
             )
             for speaker_meta in core_metas
@@ -49,15 +48,7 @@ class MetasStore:
         """
 
         core_metas = [CoreSpeaker(**speaker) for speaker in json.loads(engine.speakers)]
-        return self.combine_metas(core_metas)
-
-    @property
-    def engine_speakers_path(self) -> Path:
-        return self._engine_speakers_path
-
-    @property
-    def loaded_metas(self) -> Dict[str, EngineSpeaker]:
-        return self._loaded_metas
+        return self._combine_metas(core_metas)
 
 
 def construct_lookup(speakers: List[Speaker]) -> Dict[int, Tuple[Speaker, StyleInfo]]:
